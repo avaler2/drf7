f35b2b818d1edcfb0b4c09e9a445acd62c06ab68
paul <ppolanski@free.org.pl>
2024-05-03 13:58:09 +0200

    Added Post table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField(max_length=1000)
+    date_create = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
